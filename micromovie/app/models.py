e53a706b569264504e3db1589d84f132e1b24f91
min.tu <18273603316@163.com>
2020-12-18 16:34:12 +0800

    Default Changelist

---
+++ micromovie/app/models.py
@@ -0,0 +1,204 @@
+# coding:utf8
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pymysql
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@127.0.0.1:3306/movie"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+db = SQLAlchemy(app)
+
+
+# 会员模型
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    pwd = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    phone = db.Column(db.String(11), unique=True)
+    info = db.Column(db.Text)
+    face = db.Column(db.String(255), unique=True)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
+    uuid = db.Column(db.String(255), unique=True)
+    userlogs = db.relationship('Userlog', backref='user')  # 外键关联关系：User-Userlog.user_id
+    comments = db.relationship('Comment', backref='user')  # 外键关联关系：User-Comment.user_id
+    moviecols = db.relationship('Moviecol', backref='user')  # 外键关联关系：User-Moviecol.user_id
+
+    def __repr__(self):
+        return "<User %r>" % self.name
+
+
+# 会员登录日志模型
+class Userlog(db.Model):
+    __tablename__ = 'userlog'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    ip = db.Column(db.String(100))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
+
+    def __repr__(self):
+        return "<Userlog %r>" % self.id
+
+
+# 电影标签模型
+class Tag(db.Model):
+    __tablename__ = "tag"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.String(100)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
+    movies = db.relationship("Movie", backref='tag')  # 外键关联关系：Tag-Movie.tag_id
+
+    def __repr__(self):
+        return "<Tag %r>" % self.name
+
+
+# 电影模型
+class Movie(db.Model):
+    __tablename__ = "movie"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), unique=True)
+    url = db.Column(db.String(255), unique=True)
+    info = db.Column(db.Text)
+    logo = db.Column(db.String(255), unique=True)
+    star = db.Column(db.SmallInteger)
+    playnum = db.Column(db.BigInteger)
+    commentnum = db.Column(db.BigInteger)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
+    area = db.Column(db.String(255))
+    release_time = db.Column(db.Date)
+    length = db.Column(db.String(100))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
+    comments = db.relationship('Comment', backref='movie')  # 外键关联关系：Movie-Comment.movie_id
+    movircols = db.relationship('Moviecol', backref='movie')  # 外键关联关系：Movie-Moviecol.movie_id
+
+    def __repr__(self):
+        return "<Movie %r>" % self.title
+
+
+# 上映预告模型
+class Preview(db.Model):
+    __tablename__ = "preview"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), unique=True)
+    logo = db.Column(db.String(255), unique=True)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
+
+    def __repr__(self):
+        return "<Preview %r>" % self.title
+
+
+# 评论模型
+class Comment(db.Model):
+    __tablename__ = "comment"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
+
+    def __repr__(self):
+        return "<Comment %r>" % self.id
+
+
+# 电影收藏模型
+class Moviecol(db.Model):
+    __tablename__ = "moviecol"
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
+
+    def __repr__(self):
+        return "<Moviecol %r>" % self.id
+
+
+# 权限模型
+class Auth(db.Model):
+    __tablename__ = "auth"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    url = db.Column(db.String(255), unique=True)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
+
+    def __repr__(self):
+        return "<Auth %r>" % self.name
+
+
+# 角色模型
+class Role(db.Model):
+    __tablename__ = "role"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    auths = db.Column(db.String(600))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
+
+    def __repr__(self):
+        return "<Role %r>" % self.name
+
+
+# 管理员模型
+class Admin(db.Model):
+    __tablename__ = "admin"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    pwd = db.Column(db.String(100))
+    is_super = db.Column(db.SmallInteger)  # 超级管理员身份，0为超级管理员
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
+    adminlogs = db.relationship('Adminlog', backref='admin')  # 外键关联关系：Admin-Adminlog.admin_id
+    oplogs = db.relationship('Oplog', backref='admin')  # 外键关联关系：Admin-Oplog.admin_id
+
+    def __repr__(self):
+        return "<Admin %r>" % self.name
+
+
+# 管理员登陆日志模型
+class Adminlog(db.Model):
+    __tablename__ = "adminlog"
+    id = db.Column(db.Integer, primary_key=True)
+    admin_id = db.Column(db.Integer, db.ForeignKey('admin.id'))
+    ip = db.Column(db.String(100))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
+
+    def __repr__(self):
+        return "<Adminlog %r>" % self.id
+
+
+# 管理员操作日志
+class Oplog(db.Model):
+    __tablename__ = "oplog"
+    id = db.Column(db.Integer, primary_key=True)
+    admin_id = db.Column(db.Integer, db.ForeignKey('admin.id'))
+    ip = db.Column(db.String(100))
+    reason = db.Column(db.String(600))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
+
+    def __repr__(self):
+        return "<oplog %r>" % self.id
+
+
+if __name__ == '__main__':
+    # db.create_all() #创建表格
+    """
+    # 添加角色
+    role = Role(
+        name="超级管理员",
+        auth=""
+    )
+    db.session.add(role)
+    db.session.commit()
+    """
+
+    from werkzeug.security import generate_password_hash
+
+    admin = Admin(
+        name="super_admin",#创建超级管理员账号
+        pwd=generate_password_hash("super_admin"), #密码
+        is_super=0, #标记为超级管理员
+        role_id=1 #定义为1号角色
+    )
+    db.session.add(admin)
+    db.session.commit()
--- micromovie/manage.py
+++ micromovie/manage.py
@@ -1,5 +1,5 @@
 # coding:utf8
-from app import app_
+from app import app
 
 if __name__ == "__main__":
-    app_.run()
+    app.run()
